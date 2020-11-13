146afe35ace544d92292f60f81387bf6ce169d48
Jakub <jakub.jakubecky123@gmail.com>
2020-11-13 17:04:14 +0100

    ok, lets go!@@@2

--- main.cpp
+++ main.cpp
@@ -5,14 +5,18 @@ using namespace std;
 
 
 struct Container{
-    int bestCandidate = 0; //0 for a, 1 for b, 2 for c
+    const int id;
+    int bestCandidate = 0; //0 for a, 1 for b, 2 for c. we assume its a unless proving its not.
     int a;
     int b;
     int c;
-    Container(int a, int b, int c){
+    Container(int id, int a, int b, int c): id(id){
         this->a = a;
         this->b = b;
         this->c = c;
+    }
+    static bool compare(Container * c1, Container * c2, int category){
+        return true;
     }
 };
 vector<string> split(string str, char divider){
