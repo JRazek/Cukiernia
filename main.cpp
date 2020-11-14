ffc16e76399dd4ce4a24cd01199df35b18070377
Jakub <jakub.jakubecky123@gmail.com>
2020-11-14 13:24:08 +0100

    ok another way to go

--- main.cpp
+++ main.cpp
@@ -7,38 +7,27 @@ using namespace std;
 struct Container{
     const int id;
     int bestCandidate = 0; //0 for a, 1 for b, 2 for c. we assume its a unless proving its not.
-    int a;
-    int b;
-    int c;
+    int productsPerCategory[3];
     Container(int id, int a, int b, int c): id(id){
-        this->a = a;
-        this->b = b;
-        this->c = c;
+        this->productsPerCategory[0] = a;
+        this->productsPerCategory[1] = b;
+        this->productsPerCategory[2] = c;
     }
-    static bool compare(Container * c1, Container * c2, int category){
-        int c1CatCount;     //num of certain category
-        int c1Difference;   //num of swaps needed to achieve certain category
-
-        int c2CatCount;
-        int c2Difference;
-        switch (category) {
-            case 0: c1CatCount = c1->a; c1Difference = c1->b + c1->c; c2CatCount = c2->a; c2Difference = c2->b + c2->c; break;
-            case 1: c1CatCount = c1->b; c1Difference = c1->a + c1->c; c2CatCount = c2->b; c2Difference = c2->a + c2->c; break;
-            case 2: c1CatCount = c1->c; c1Difference = c2->a + c2->b; c2CatCount = c2->c; c2Difference = c2->a + c2->b; break;
-            default: cout<<"Error"; return false;
+    int getEffortForCategory(int catNum){
+        switch (catNum) {
+            case 0: return productsPerCategory[1] + productsPerCategory[2];
+            case 1: return productsPerCategory[0] + productsPerCategory[2];
+            case 2: return productsPerCategory[0] + productsPerCategory[1];
+            default: cout<<"ERROR"; return -1;
         }
-        if(c1Difference != c2Difference){
-            return c1Difference < c2Difference;
-        }
-        return c1CatCount > c2CatCount;
     }
 };
 vector<string> split(string str, char divider){
     vector<string> result;
 
-    string currWord = "";
+    string currWord;
     for(int i = 0; i < str.size(); i ++){
-        currWord+=str[i];
+        currWord += str[i];
         if(str[i] == divider || str.size()-1 == i){
             result.push_back(currWord);
             currWord = "";
@@ -47,6 +36,7 @@ vector<string> split(string str, char divider){
     return result;
 }
 
+
 int main() {
     string line;
     getline(cin, line);
@@ -54,15 +44,47 @@ int main() {
     int containersCount = stoi(line);
 
     vector<Container *> containers;
+
+    bool catPresent[3] = {false};
+
     for(int i = 0; i < containersCount; i ++){
         getline(cin, line);
         vector<string> args = split(line, ' ');
         int a = stoi(args[0]);
         int b = stoi(args[1]);
         int c = stoi(args[2]);
+
+        catPresent[0] += a;
+        catPresent[1] += b;
+        catPresent[2] += c;
+
         containers.push_back(new Container(i, a, b , c));
     }
 
+    int catContainers[3] = {0};
+    catContainers[0] = containersCount;//all the containers are default to a
+
+    for(int cat = 1; cat <= 2; cat++) {
+        for (int i = 0; i < containersCount; i++) {
+            Container * c = containers[i];
+            if(c->getEffortForCategory(c->bestCandidate) > c->getEffortForCategory(cat)){
+                catContainers[c->bestCandidate] -- ;
+                c->bestCandidate = cat;
+                catContainers[cat] ++;
+            }
+        }
+    }
+
+
+
+
+
+    int sum = 0;
+    for(int i = 0; i < containersCount; i ++){
+        Container * c = containers[i];
+        sum += c->getEffortForCategory(c->bestCandidate);
+    }
+    cout<<sum;
     for(auto c : containers){
         delete c;
     }
